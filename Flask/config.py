1d2133f30c52045cd7fa72971ea1ffd2807f2cac
zhonglanwen <zhonglanwen@gmail.com>
2018-07-22 20:58:25 +0800

    add files

---
+++ Flask/config.py
@@ -0,0 +1,13 @@
+class Config(object):
+    """Base config class"""
+    pass
+
+class ProdConfig(Config):
+    """Production config class"""
+    pass
+
+class DevConfig(Config):
+    """Development config class."""
+    #Open the DEBUG
+    DEBUG = True
+
